1d688860e3cbc7f47ed381b9b51b3b2e2e9ca542
Jury Verrigni <jury.verrigni@gmail.com>
2017-07-10 10:29:56 +0100

    Adds settings to navbar

--- client/dev/dashboard/components/sidebar/sidebar-routes.config.ts
+++ client/dev/dashboard/components/sidebar/sidebar-routes.config.ts
@@ -2,11 +2,7 @@ import {  RouteInfo } from './sidebar.metadata';
 
 export const ROUTES: RouteInfo[] = [
     { path: '/', title: 'Home',  icon: 'dashboard', class: 'active' },
-    { path: 'user', title: 'User Profile',  icon:'person', class: '' },
-    { path: 'table', title: 'Table List',  icon:'content_paste', class: '' },
-    { path: 'typography', title: 'Typography',  icon:'library_books', class: '' },
-    { path: 'icons', title: 'Icons',  icon:'bubble_chart', class: '' },
-    { path: 'maps', title: 'Maps',  icon:'location_on', class: '' },
-    { path: 'notifications', title: 'Notifications',  icon:'notifications', class: '' },
-    { path: 'upgrade', title: 'Upgrade to PRO',  icon:'unarchive', class: 'active-pro' },
+    { path: '/settings/api', title: 'API',  icon:'cloud', class: '' },
+    { path: '/settings/plants', title: 'Plants Settings',  icon:'filter_vintage', class: '' },
+    { path: 'user', title: 'User Profile',  icon:'person', class: '' }
 ];
--- client/dev/settings/components/settings-route.ts
+++ client/dev/settings/components/settings-route.ts
@@ -6,11 +6,23 @@ import {
 import {
 	SettingsCmp
 } from "./settings-cmp";
+import { SettingsApiCmp } from "./settings-api";
+import { SettingsPlantsCmp } from "./settings-plants";
 
 const settingsRoutes:Routes = [
 	{
 		path: "settings",
 		component: SettingsCmp,
+		pathMatch: "full"
+	}, 
+	{
+		path: "settings/api",
+		component: SettingsApiCmp,
+		pathMatch: "full"
+	},
+	{
+		path: "settings/plants",
+		component: SettingsPlantsCmp,
 		pathMatch: "full"
 	}
 ]
