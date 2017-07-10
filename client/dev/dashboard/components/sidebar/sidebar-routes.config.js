f9fd6367e6ef21f9560ca86e244f7844af62a65c
Jury Verrigni <jury.verrigni@gmail.com>
2017-07-10 10:41:44 +0100

    Late spring cleaning

--- client/dev/dashboard/components/sidebar/sidebar-routes.config.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ROUTES = [
-    { path: '/', title: 'Home', icon: 'dashboard', class: 'active' },
-    { path: '/settings/api', title: 'API', icon: 'cloud', class: '' },
-    { path: '/settings/plants', title: 'Plants Settings', icon: 'filter_vintage', class: '' },
-    { path: 'user', title: 'User Profile', icon: 'person', class: '' }
-];
--- tests/client/settings/components/settings_test.js
+++
@@ -1,6 +0,0 @@
-describe('myComponent', function () {
-    beforeEach(function () {
-    });
-    describe("creation", function () {
-    });
-});
